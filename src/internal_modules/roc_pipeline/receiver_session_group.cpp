roc-streaming/roc-toolkit

Rebase ring buffer branch to develop

src/internal_modules/roc_pipeline/receiver_session_group.cpp
@@ -166,11 +166,54 @@ ReceiverSessionGroup::route_transport_packet_(const packet::PacketPtr& packet) {
     }
 
     if (!can_create_session_(packet)) {
+        enqueue_prebuf_packet_(packet);
         // TODO(gh-183): return status
         return status::StatusOK;
     }
 
     return create_session_(packet);
+}
+
+void ReceiverSessionGroup::enqueue_prebuf_packet_(const packet::PacketPtr& packet_ptr) {
+    prebuf_packets_.push_back(*packet_ptr.get());
+
+    core::nanoseconds_t now = core::timestamp(core::ClockMonotonic);
+
+    while (prebuf_packets_.size() > 0) {
+        core::nanoseconds_t received = prebuf_packets_.front()->udp()->receive_timestamp;
+        if (now - received > receiver_config_.default_session.prebuf_len) {
+            prebuf_packets_.remove(*prebuf_packets_.front());
+        } else {
+            break;
+        }
+    }
+}
+
+void ReceiverSessionGroup::dequeue_prebuf_packets_(ReceiverSession& sess) {
+    packet::PacketPtr curr, next;
+
+    if (prebuf_packets_.size() == 0) {
+        return;
+    }
+
+    core::nanoseconds_t now = core::timestamp(core::ClockMonotonic);
+
+    for (curr = prebuf_packets_.front(); curr; curr = next) {
+        next = prebuf_packets_.nextof(*curr);
+
+        // if packet is too old, remove it from the queue
+        core::nanoseconds_t received = curr->udp()->receive_timestamp;
+        if (now - received > receiver_config_.default_session.prebuf_len) {
+            prebuf_packets_.remove(*curr);
+            continue;
+        }
+
+        // if session handles the packet, remove it from the queue
+        const status::StatusCode code = sess.route(curr);
+        if (code == status::StatusOK) {
+            prebuf_packets_.remove(*curr);
+        }
+    }
 }
 
 status::StatusCode
@@ -251,6 +294,8 @@ ReceiverSessionGroup::create_session_(const packet::PacketPtr& packet) {
     sessions_.push_back(*sess);
 
     receiver_state_.add_sessions(+1);
+
+    dequeue_prebuf_packets_(*sess);
 
     return status::StatusOK;
 }

anatmiller/roc-streaming--opensource

src/internal_modules/roc_pipeline/receiver_session_group.cpp
@@ -328,6 +328,7 @@ ReceiverSessionGroup::route_transport_packet_(const packet::PacketPtr& packet) {
     }
 
     if (sess) {
+        enqueue_prebuf_packet_(packet);
         // Session found, route packet to it.
         return sess->route_packet(packet);
     }
@@ -338,6 +339,48 @@ ReceiverSessionGroup::route_transport_packet_(const packet::PacketPtr& packet) {
     }
 
     return status::StatusNoRoute;
+}
+
+void ReceiverSessionGroup::enqueue_prebuf_packet_(const packet::PacketPtr& packet_ptr) {
+    prebuf_packets_.push_back(*packet_ptr.get());
+
+    core::nanoseconds_t now = core::timestamp(core::ClockMonotonic);
+
+    while (prebuf_packets_.size() > 0) {
+        core::nanoseconds_t received = prebuf_packets_.front()->udp()->receive_timestamp;
+        if (now - received > source_config_.session_defaults.prebuf_len) {
+            prebuf_packets_.remove(*prebuf_packets_.front());
+        } else {
+            break;
+        }
+    }
+}
+
+void ReceiverSessionGroup::dequeue_prebuf_packets_(ReceiverSession& sess) {
+    packet::PacketPtr curr, next;
+
+    if (prebuf_packets_.size() == 0) {
+        return;
+    }
+
+    core::nanoseconds_t now = core::timestamp(core::ClockMonotonic);
+
+    for (curr = prebuf_packets_.front(); curr; curr = next) {
+        next = prebuf_packets_.nextof(*curr);
+
+        // if packet is too old, remove it from the queue
+        core::nanoseconds_t received = curr->udp()->receive_timestamp;
+        if (now - received > source_config_.session_defaults.prebuf_len) {
+            prebuf_packets_.remove(*curr);
+            continue;
+        }
+
+        // if session handles the packet, remove it from the queue
+        const status::StatusCode code = sess.route_packet(curr);
+        if (code == status::StatusOK) {
+            prebuf_packets_.remove(*curr);
+        }
+    }
 }
 
 status::StatusCode
@@ -430,6 +473,8 @@ ReceiverSessionGroup::create_session_(const packet::PacketPtr& packet) {
 
     sessions_.push_back(*sess);
     state_tracker_.register_session();
+
+    dequeue_prebuf_packets_(*sess);
 
     return status::StatusOK;
 }